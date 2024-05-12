99ed9490e9da3f8c775aa03b06f613971ed85f14
Rusty Pickle <rusty.pickle94@gmail.com>
2024-05-13 03:59:29 +0600

    Update formatting + add Wakatime

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,15 +1,15 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = true,
-  keys = {
-    {
-      "<leader>uh",
-      "<cmd>ToggleTerm size=10 direction=horizontal<cr>",
-      desc = "Open a horizontal terminal at the Desktop directory",
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    keys = {
+        {
+            "<leader>uh",
+            "<cmd>ToggleTerm size=10 direction=horizontal<cr>",
+            desc = "Open a horizontal terminal at the Desktop directory",
+        },
     },
-  },
-  opts = {
-    shade_terminals = false,
-  },
+    opts = {
+        shade_terminals = false,
+    },
 }
