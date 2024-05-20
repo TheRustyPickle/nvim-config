c52935f31eb198715516aac5c0f5285a34030f34
Rusty Pickle <rusty.pickle94@gmail.com>
2024-05-21 01:12:57 +0600

    Update indentation stuff

---
+++ lua/plugins/blankline.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = {
+            indent = {
+                char = "┃",
+                tab_char = "┃",
+            },
+            scope = {
+                enabled = true,
+                show_start = true,
+                show_end = true,
+            },
+        },
+    },
+}
