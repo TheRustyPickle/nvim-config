0437d239d73ce704bf0e957bae9957c396b89174
Rusty Pickle <rusty.pickle94@gmail.com>
2024-05-21 01:12:57 +0600

    Update indentation stuff

---
+++ lua/plugins/minindent.lua
@@ -0,0 +1,7 @@
+return {
+    "echasnovski/mini.indentscope",
+    opts = {
+        symbol = "┃",
+        options = { try_as_border = true },
+    },
+}
--- lua/plugins/spell.lua
+++ lua/plugins/spell.lua
@@ -1,16 +1,12 @@
 return {
     "mfussenegger/nvim-lint",
-  event = "LazyFile",
-  opts = {
-    -- Event to trigger linters
-    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-    linters_by_ft = {
-      fish = { "fish" },
-      -- Use the "*" filetype to run linters on all filetypes.
-      -- ['*'] = { 'global linter' },
-      -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
-      -- ['_'] = { 'fallback linter' },
-       ["*"] = { "typos" },
+    event = "LazyFile",
+    opts = {
+        -- Event to trigger linters
+        events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+        linters_by_ft = {
+            fish = { "fish" },
+            ["*"] = { "typos" },
+        },
     },
-}
 }
