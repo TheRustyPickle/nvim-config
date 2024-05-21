3afc8024bb3678e3c21f8bb02fc3721ad2682e7c
Rusty Pickle <rusty.pickle94@gmail.com>
2024-05-22 04:24:14 +0600

    Add config for neovide

--- init.lua
+++ init.lua
@@ -1,2 +1,20 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+    vim.o.guifont = "JetBrains Mono:h11" -- text below applies for VimScript
+    vim.g.neovide_theme = "light"
+    vim.g.neovide_refresh_rate = 144
+    vim.g.neovide_cursor_smooth_blink = true
+    vim.g.neovide_underline_stroke_scale = 2.0
+    vim.g.neovide_cursor_animate_in_insert_mode = true
+    vim.g.neovide_cursor_animate_command_line = true
+    vim.g.neovide_remember_window_size = true
+    vim.opt.guicursor = {
+        "n-v-c:block", -- Normal, Visual, Command modes: block cursor
+        "i-ci-ve:ver25", -- Insert, Command-line insert, Visual-execute modes: vertical bar with 25% width
+        "r-cr:hor20", -- Replace, Command-line replace modes: horizontal bar with 20% height
+        "o:hor50",
+        "a:blinkwait400-blinkon400-blinkoff400", -- All modes: set blinking timing
+    }
+end
