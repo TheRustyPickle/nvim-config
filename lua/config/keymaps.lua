c5375ce357f04f1ed00e2ccf77cf40b1f88de19d
Rusty Pickle <rusty.pickle94@gmail.com>
2024-05-26 04:10:01 +0600

    Add keymap for pasting with ctrl shift v

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+if vim.g.neovide then
+    vim.keymap.set("n", "<sc-v>", '"+P') -- Paste normal mode
+    vim.keymap.set("v", "<sc-v>", '"+P') -- Paste visual mode
+    vim.keymap.set("c", "<sc-v>", "<C-R>+") -- Paste command mode
+    vim.keymap.set("i", "<sc-v>", '<ESC>l"+Pli') -- Paste insert mode
+    vim.api.nvim_set_keymap("t", "<sc-v>", '<C-\\><C-n>"+Pi', { noremap = true }) -- Paste in terminal mode, specifically added for lazy git
+end
