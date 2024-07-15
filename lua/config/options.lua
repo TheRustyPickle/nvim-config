4cbd2703e770ea484469321af6ce5669911c1ad6
Rusty Pickle <rusty.pickle94@gmail.com>
2024-07-15 17:11:37 +0600

    Add key to restart rust analyzer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,6 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.g.autoformat = false
 vim.opt.scrolloff = 15
+
+local map = vim.keymap.set
+map("n", "<leader>cz", ":RustAnalyzer restart<CR>", { desc = "Restart Rust Analyzer", silent = true })
