e3b5ee65a84f67514456bccfd63611aed8ff55af
Rusty Pickle <rusty.pickle94@gmail.com>
2024-08-10 17:31:06 +0600

    Update

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,7 +2,7 @@ return {
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "everforest",
+            colorscheme = "bluloco",
         },
     },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
