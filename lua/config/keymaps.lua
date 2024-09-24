467659e41f7ed2b8a1ea1e51e3b2378bac09b073
Rusty Pickle <rusty.pickle94@gmail.com>
2024-09-24 21:25:45 +0600

    Update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,18 @@ if vim.g.neovide then
     vim.keymap.set("i", "<sc-v>", '<ESC>l"+Pli') -- Paste insert mode
     vim.api.nvim_set_keymap("t", "<sc-v>", '<C-\\><C-n>"+Pi', { noremap = true }) -- Paste in terminal mode, specifically added for lazy git
 end
+
+local map = vim.keymap.set
+map("n", "<leader>cz", ":RustAnalyzer restart<CR>", { desc = "Restart Rust Analyzer", silent = true })
+map("n", "<leader>cx", ":RustAnalyzer start<CR>", { desc = "Start Rust Analyzer", silent = true })
+map("n", "<leader>ghv", ":DiffviewOpen<CR>", { desc = "Start DiffView", silent = true })
+map("n", "<leader>ghV", ":lua open_diffview_with_branch()<CR>", { desc = "Start DiffView on Branch", silent = true })
+
+function _G.open_diffview_with_branch()
+    local branch_name = vim.fn.input("Enter branch name: ")
+    if branch_name ~= "" then
+        vim.cmd("DiffviewOpen origin/" .. branch_name .. "...HEAD")
+    else
+        print("No branch name entered.")
+    end
+end
