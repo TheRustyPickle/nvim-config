d6c7a27e55ac1fd0eaf033409be9e61779c74283
Rusty Pickle <rusty.pickle94@gmail.com>
2024-12-16 21:41:54 +0600

    Update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,9 +5,10 @@
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.g.snacks_animate = true
 vim.g.autoformat = false
 vim.opt.scrolloff = 15
 vim.g.lazyvim_python_ruff = "ruff"
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.background = "light"
-
+vim.g.autoformat = false
--- lua/plugins/lsp.lua
+++
@@ -1,1 +0,0 @@
-return { "neovim/nvim-lspconfig", opts = { opts = { inlay_hints = { enabled = true } } } }
