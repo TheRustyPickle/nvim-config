aa055fcd288c2c3bfe382797f9fdfd6a74509fb3
Rusty Pickle <rusty.pickle94@gmail.com>
2024-12-26 20:48:03 +0600

    Add neovide color settings

--- init.lua
+++ init.lua
@@ -21,4 +21,20 @@ if vim.g.neovide then
         "r-cr:hor20", -- Replace, Command-line replace modes: horizontal bar with 20% height
         "a:blinkwait400-blinkon400-blinkoff400", -- All modes: set blinking timing
     }
+    vim.g.terminal_color_0 = "#5c5f77"
+    vim.g.terminal_color_1 = "#d20f39"
+    vim.g.terminal_color_2 = "#40a02b"
+    vim.g.terminal_color_3 = "#df8e1d"
+    vim.g.terminal_color_4 = "#1e66f5"
+    vim.g.terminal_color_5 = "#ea76cb"
+    vim.g.terminal_color_6 = "#179299"
+    vim.g.terminal_color_7 = "#acb0be"
+    vim.g.terminal_color_8 = "#6c6f85"
+    vim.g.terminal_color_9 = "#d20f39"
+    vim.g.terminal_color_10 = "#40a02b"
+    vim.g.terminal_color_11 = "#df8e1d"
+    vim.g.terminal_color_12 = "#1e66f5"
+    vim.g.terminal_color_13 = "#ea76cb"
+    vim.g.terminal_color_14 = "#179299"
+    vim.g.terminal_color_15 = "#bcc0cc"
 end
