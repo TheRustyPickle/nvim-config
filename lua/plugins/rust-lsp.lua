75b646d5d73d3c13d871eb77830da12fa52abb22
Rusty Pickle <rusty.pickle94@gmail.com>
2024-12-26 20:48:07 +0600

    Update

--- lua/plugins/rust-lsp.lua
+++ lua/plugins/rust-lsp.lua
@@ -1,4 +1,20 @@
 return {
     "mrcjkb/rustaceanvim",
     version = "^5",
+    opts = {
+        server = {
+            default_settings = {
+                ["rust-analyzer"] = {
+                    procMacro = {
+                        enable = true,
+                        ignored = {
+                            ["async-trait"] = { "" },
+                            ["napi-derive"] = { "" },
+                            ["async-recursion"] = { "" },
+                        },
+                    },
+                },
+            },
+        },
+    },
 }
