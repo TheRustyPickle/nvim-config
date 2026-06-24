a598e2387c39680706fd88efc57ea5bcf78fb94e
Rusty Pickle <rusty.pickle94@gmail.com>
2026-06-24 16:01:14 +0600

    Add jq, package-info.nvim markdown renderer

---
+++ lua/plugins/package-info.lua
@@ -0,0 +1,19 @@
+return {
+    "vuki656/package-info.nvim",
+    opts = {
+        notifications = false,
+        package_manager = "pnpm",
+
+        highlights = {
+            up_to_date = {
+                fg = "#1e66f5",
+            },
+            outdated = {
+                fg = "#fe640b",
+            },
+            invalid = {
+                fg = "#d20f39",
+            },
+        },
+    },
+}
